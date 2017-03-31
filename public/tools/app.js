7be9b8b5ea878bb9bae1da3f944211570ab2c946
Abhijit <abhijit_mallik@gensler.com>
2017-03-31 17:57:43 +0530

    added space to change the code

--- public/tools/app.js
+++ public/tools/app.js
@@ -12,8 +12,8 @@ if(!this.webrtc.app){
 	var yourConnection ="";
 	var otherUser = "";
 	var remoteVideo = "";
-	connection = new WebSocket('ws://'+window.location.host);
-	$(".login-div").append(this.webrtc.views.login);
+	connection = new WebSocket('wss://'+window.location.host);
+	$(".login-div").append(this.webrtc.views.login);  
 
     var send = function(message){
 		if (connectedUser) {
